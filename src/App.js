eb24785c07e26c6222b45306fed71b58912aafc3
Ramith <89182652+ramithperera@users.noreply.github.com>
2024-02-24 17:50:35 +0530

    url change

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
           "Content-Type": "application/json",
         },
       };
-      const response = await fetch("http://localhost:8000/gemini", options);
+      const response = await fetch("https://react-ai-chat-bot.vercel.app/gemini", options);
       const data = await response.text();
       console.log(data);
       setChatHistory((oldChatHistory) => [
